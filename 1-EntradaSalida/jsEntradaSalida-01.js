1bf00fdfb15c58de572562011ca403869f52fa80
Joaquín893 <joacoferre13@gmail.com>
2020-07-15 12:10:18 -0300

    Los 5 primeros

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -2,7 +2,11 @@
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
-{
-	alert("Nose que poner");
+{   //Comentario de línea
+	alert("Esto funciona de maravilla"); // Esto muestra un mensaje de alerta
+	/* 
+	Comentario de bloque
+    */
 }
+
 
--- 1-EntradaSalida/jsEntradaSalida-05.js
+++ 1-EntradaSalida/jsEntradaSalida-05.js
@@ -3,6 +3,23 @@ Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	alert("ok");
+	var nombreIngresado;
+	var edadIngresada;
+
+	nombreIngresado=txtIdNombre.value;
+	edadIngresada=txtIdEdad.val
+
+	//testing 
+	alert("Usted se llama "+nombreIngresado+" y tiene "+edadIngresada+" años");
+
+	/*
+
+	alert("Usted se llama "edadIngresada);
+	alert("y tiene "+nombreIngresado+" años");
+
+	*/
+
+	//alert("Usted se llama jose y tiene 66 años");
+
 }
 
