e11cf6128b7da93a029d4c56e44a389aa04e2831
Joaquín893 <joacoferre13@gmail.com>
2020-07-16 15:12:08 -0300

    Cambio en js 1

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,7 +3,7 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {   //Comentario de línea
-	alert("Esto funciona de maravilla"); // Esto muestra un mensaje de alerta
+	alert("Esto funciona de maravillas"); // Esto muestra un mensaje de alerta
 	/* 
 	Comentario de bloque
     */
